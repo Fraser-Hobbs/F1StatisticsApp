a580cd70b536a78ea0a7fc375174dc85c506ca8e
Fraser <fraser@fraserhobbs.dev>
2024-12-10 01:14:47 +0000

    chore: Rename main.scala to F1StatisticsApp.scala and scaffold application structure
    - Renamed main.scala to F1StatisticsApp.scala for better clarity and alignment with the application's purpose.
    - Updated main entry point:
      - Changed `@main` function to an object-based structure using `F1StatisticsApp` extending `App`.
      - Added placeholders (`TODOs`) for core functionalities, including dataset loading, menu handling, and analysis.
    - Introduced structured scaffolding:
      - `MenuHandler` object for managing user interactions and menu navigation.
      - `DataHandler` object for loading and parsing the F1 dataset.
      - `DataAnalyser` object for implementing core analysis functionalities.
    
    Signed-off-by: Fraser <fraser@fraserhobbs.dev>

---
+++ src/F1StatisticsApp.scala
@@ -0,0 +1,67 @@
+
+
+
+object F1StatisticsApp extends App {
+
+  @main
+  def main(): Unit = {
+    println("Welcome to the F1 Statistics CLI Application")
+
+    // TODO -  Load f1 Dataset
+
+    // TODO - Check Dataset loaded (fail/pass)
+
+    // TODO - Pass - Load Menu
+
+    // TODO - Fail - Display Message and Exit
+  }
+
+}
+
+object MenuHandler {
+  // TODO - Display the main menu options to the user
+
+  // TODO - Use a tail-recursive function to handle user input and menu navigation
+
+  // TODO - Map menu options to corresponding analysis functions from DataAnalyser
+
+  // TODO - Handle invalid menu choices and prompt the user to try again
+
+  // TODO - Add an option to gracefully exit the application
+}
+
+object DataHandler {
+
+  // TODO - Create type aliases for F1Data and F1Driver
+
+  // TODO - Load Data from file
+
+  // TODO - Parse F1 Data
+}
+
+object DataAnalyser {
+
+  // TODO - Analysis 1 - Get winners by year
+
+  // TODO - Analysis 1 - Display winners by year
+
+  // TODO - Analysis 2 - Get results for a specific season
+
+  // TODO - Analysis 2 - Display results for a specific season
+
+  // TODO - Analysis 3 - Get total number of races each season/year
+
+  // TODO - Analysis 3 - Display total number of races each season/year
+
+  // TODO - Analysis 4 - Get average points per season
+
+  // TODO - Analysis 4 - Display average points per season
+
+  // TODO - Analysis 5 - Get total points by season
+
+  // TODO - Analysis 5 - Display total points by season (ascending)
+
+  // TODO - Analysis 6 - Get total points for a specific driver
+
+  // TODO - Analysis 6 - Display total points for a specific driver
+}
